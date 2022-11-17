faf4da22cb11c6f87382c78f4de03fef57193bec
ajaykanniyappan <ajaykanniyappan@gmail.com>
2022-11-17 15:12:01 +0530

    Vitejs Configurations

--- configs/vite.config.ts
+++ configs/vite.config.ts
@@ -1,47 +1,45 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import EnvironmentPlugin from 'vite-plugin-environment';
-import eslintPlugin from 'vite-plugin-eslint';
-import { resolve } from 'path';
 import { builtinModules } from 'module';
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import Electron from 'vite-plugin-electron';
+import EnvironmentPlugin from 'vite-plugin-environment';
+import React from '@vitejs/plugin-react';
+import TsConfigPaths from 'vite-tsconfig-paths';
 import { port } from '../DevConfig.json';
 
 export default defineConfig({
   base: './',
-  root: resolve('./src/renderer'),
-  publicDir: resolve('./src/renderer/public'),
   clearScreen: false,
+  publicDir: resolve('./src/renderer/public'),
+  root: resolve('./src/renderer'),
+  server: {
+    port,
+  },
   build: {
-    assetsDir: '', // See: https://github.com/electron-vite/electron-vite-vue/issues/287
+    assetsDir: '',
     outDir: resolve('./app/dist/renderer'),
   },
   plugins: [
+    React(),
     EnvironmentPlugin('all', { prefix: '' }),
-    eslintPlugin(),
-    electron({
-      entry: ['src/main/main.ts', 'src/main/preload.ts'],
+    TsConfigPaths({ projects: [resolve(__dirname, '../tsconfig.json')] }),
+    Electron({
+      entry: [resolve('src/main/main.ts'), resolve('src/main/preload.ts')],
       onstart: (options) => {
         options.startup(['.', '--inspect=5858', '--remote-debugging-port=9227']);
       },
       vite: {
-        plugins: [
-          EnvironmentPlugin('all', { prefix: '' }),
-          tsconfigPaths({ projects: [resolve(__dirname, '../tsconfig.json')] }),
-        ],
         build: {
-          assetsDir: '.',
-          outDir: 'app/dist/main',
+          assetsDir: '',
+          outDir: resolve('./app/dist/main'),
           rollupOptions: { external: ['electron', ...builtinModules] },
         },
+        plugins: [
+          EnvironmentPlugin('all', { prefix: '' }),
+          TsConfigPaths({ projects: [resolve(__dirname, '../tsconfig.json')] }),
+        ],
       },
     }),
-    react(),
-    tsconfigPaths({ projects: [resolve(__dirname, '../tsconfig.json')] }),
   ],
-  server: {
-    port,
-  },
 });
